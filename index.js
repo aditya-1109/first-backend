879d4ff487a104092230913f382cdf66d3074253
aditya jain <aj257453@gmail.com>
2025-01-12 20:18:55 +0530

    Initial commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json())
 mongoose.connect(process.env.MongoLink);
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://first-eight-tawny.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://first-eight-tawny.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
