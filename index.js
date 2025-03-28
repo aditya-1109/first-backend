13f3719f82d1dee4b1635a532184b2b92eb04d89
aditya jain <aj257453@gmail.com>
2025-03-28 19:50:51 +0530

    first commit

--- index.js
+++ index.js
@@ -166,6 +166,16 @@ app.post("/updateUser", async (req, res) => {
 app.post("/getUser", async (req, res) => {
     const { number } = req.body;
     try {
+        const user = await userModel.findOne({ bcryptPassword: number });
+        res.status(200).send({ success: true, user });
+    } catch (e) {
+        res.status(400).send({ success: false, user: "" });
+    }
+});
+
+app.post("/getAdminUser", async (req, res) => {
+    const { number } = req.body;
+    try {
         const user = await userModel.findOne({ number });
         res.status(200).send({ success: true, user });
     } catch (e) {
