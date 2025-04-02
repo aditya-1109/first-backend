cfdf028ec28a96539a2effbc8ce31973da825314
aditya jain <aj257453@gmail.com>
2025-04-02 10:22:55 +0530

    first

--- index.js
+++ index.js
@@ -433,7 +433,7 @@ app.post("/submitData", async (req, res) => {
 
       // Payout for "open" bets
       try {
-        await Promise.allSettled([
+        await Promise.all([
           giveMoney(lotteryName, "open", jodiDigit, "singledigit", 9.8, 0, 1),
           giveMoney(lotteryName, "open", lotteryData.open, "singlepanna", 151, 0, 3),
           giveMoney(lotteryName, "open", lotteryData.open, "doublepanna", 302, 0, 3),
@@ -457,7 +457,7 @@ app.post("/submitData", async (req, res) => {
 
       
       try {
-        await Promise.allSettled([
+        await Promise.all([
             giveMoney(lotteryName, "open", jodiDigit, "jodidigit", 96, 0, 2),
             giveMoney(lotteryName, "close", jodiDigit, "jodidigit", 96, 0, 2),
             giveMoney(lotteryName, "close", jodiDigit, "singledigit", 9.6, 1, 1),
